4f921b362a404df4f7e66fd60d3c7386445103c3
EduardoVisconti <duduhsg@gmail.com>
2022-06-17 15:19:52 -0500

    aula02

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 const robotron = document.querySelector('#robotron')
 
-robotron.addEventListener('click', evento => {
+robotron.addEventListener('click', (evento) => {
   console.log(evento)
 })
 
 function dizOi(nome) {
-  console.log('Oi' + nome)
+  console.log('Oi ' + nome)
   console.log('Bem-vindo ao Robotron 2000')
 }
 
-dizOi('Eduardo')
+dizOi('duds')
