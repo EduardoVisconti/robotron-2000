e212320ea71578c01eca4feb8a91f8ae72948d87
EduardoVisconti <duduhsg@gmail.com>
2022-06-18 09:45:13 -0500

    aula04

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
-const subtrair = document.querySelector('#subtrair')
-const somar = document.querySelector('#somar')
-const braco = document.querySelector('#braco')
+const controle = document.querySelectorAll('[data-controle]')
+
+controle.forEach(elemento => {
+  elemento.addEventListener('click', evento => {
+    manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+  })
+})
+
+function manipulaDados(operacao, controle) {
+  const peca = controle.querySelector('[data-controle]')
+
+  if (operacao === '-') {
+    peca.value = parseInt(peca.value) - 1
+  } else {
+    peca.value = parseInt(peca.value) + 1
+  }
+}
