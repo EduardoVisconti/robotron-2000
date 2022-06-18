22e41ef7d47d7bfefafb97c752c180393d8fb982
EduardoVisconti <duduhsg@gmail.com>
2022-06-18 17:28:44 -0500

    Update main.js

--- exercicios/aula03/ex02/main.js
+++ exercicios/aula03/ex02/main.js
@@ -6,3 +6,24 @@ document.querySelector("#botao").addEventListener("click", () => {
 document.querySelector(".close").addEventListener("click", () => {
   lista.setAttribute("data-lista", "esconder");
 });
+
+/*
+
+const button = document.querySelectorAll('[data-Aluratintas]')
+button.forEach(element => element.addEventListener('click', (event) => {
+    if (event.target.dataset.mostratinta == 'mostra') return document.querySelector('.lista').style.display = 'block'
+    document.querySelector('.lista').style.display = 'none'
+}));
+
+----
+
+const lista = document.querySelector("ul");
+document.querySelector("#botao").addEventListener("click", () => {
+  lista.setAttribute("data-lista", "mostrar");
+});
+
+document.querySelector(".close").addEventListener("click", () => {
+  lista.setAttribute("data-lista", "esconder");
+});
+
+*/
